b5411d8ece20e30b9666487112be5f18be3fb3f6
Riyaz Ali <riyaz@mergestat.com>
2023-03-17 14:02:57 +0530

    sync: add git blame syncer

--- syncs/git/blame/blame.py
+++ syncs/git/blame/blame.py
@@ -1,21 +1,35 @@
 from datetime import datetime, timezone, timedelta
 import json
-import subprocess
 import re
+import subprocess
+import sys
 
-re_match_commit = re.compile('^([a-f0-9]{40}) ([0-9]+) .*$')
+re_match_commit = re.compile('^([a-f0-9]{40}) .*$')
 re_match_author = re.compile('^author (.*)$')
 re_match_author_mail = re.compile('^author-mail <(.*)>$')
 re_match_author_time = re.compile('^author-time ([0-9]+)$')
 re_match_author_timezone = re.compile('^author-tz (-?[0-9]+)$')
 
 
-def parse_line_porcelain(line):
+def buffer_is_binary(ptr: bytes, size: int) -> bool:
+    """
+    buffer_is_binary detects if data in the buffer is a binary value 
+    based on: http://git.kernel.org/cgit/git/git.git/tree/xdiff-interface.c?id=HEAD#n198
+    """
+
+    FIRST_FEW_BYTES = 8000
+    if FIRST_FEW_BYTES < size:
+        size = FIRST_FEW_BYTES
+    
+    return b'\x00' in ptr[:size]
+
+
+def parse_line_porcelain(line: str):
     """parse_line_porcelain parses a line from source file alongwith all the headers"""
 
     match = re_match_commit.match(line)
     if match:
-        return { 'hash': match.group(1), 'line_number': int(match.group(2)) }
+        return { 'hash': match.group(1) }
     
     match = re_match_author.match(line)
     if match:
@@ -35,7 +49,7 @@ def parse_line_porcelain(line):
 
     return None
 
-def git_blame_porcelain(path_to_file):
+def git_blame_porcelain(path_to_file: str):
     """git_blame_porcelain parses the git blame for the given file"""
 
     cmd = ["git", "blame", "--line-porcelain", path_to_file]
@@ -45,26 +59,42 @@ def git_blame_porcelain(path_to_file):
         raise subprocess.CalledProcessError(p.returncode, cmd, stderr)
     output_lines = stdout.decode("utf-8").split("\n")
 
+    n = 1
     current = {}
     for line in output_lines:
         parsed_line = parse_line_porcelain(line)
         if parsed_line is not None:
             current.update(parsed_line)
         elif len(line) > 0 and line[0] == '\t': # if line starts with a tab character then its the data line
-            current.update({ 'line': line.strip() })
+            current.update({ 'line_number': n, 'line': line.strip() })
             yield current
+            n += 1
             current = {}
     
     if current:
         yield current
 
-for info in git_blame_porcelain("cmd/worker/main.go"):
-    delta = info['timezone'] if info.get('timezone') else 0
 
-    offset_hours = delta // 100
-    offset_minutes = delta % 100
-    tz = timezone(timedelta(hours=offset_hours, minutes=offset_minutes))
+if __name__ == "__main__":
+    path = sys.argv[1]
+    try:
+        with open(path, 'rb') as file:
+            buffer = file.read(8000) # to determine if the file is binary or not
+            if buffer_is_binary(buffer, len(buffer)):
+                print(f'skipping binary file "{path}"', file=sys.stderr)
+                sys.exit(0)
+            
+            for info in git_blame_porcelain(path):
+                delta = info['timezone'] if info.get('timezone') else 0
 
-    info['time'] = datetime.fromtimestamp(info['time'], tz).isoformat()
-    print(json.dumps(info))
+                offset_hours = delta // 100
+                offset_minutes = delta % 100
+                tz = timezone(timedelta(hours=offset_hours, minutes=offset_minutes))
+                isotime = datetime.fromtimestamp(info['time'], tz).isoformat()
 
+                info.update({ 'path': path, 'time': isotime })
+                print(json.dumps(info))
+                
+    except FileNotFoundError:
+        print(f'file "{path}" not found; skipping', file=sys.stderr)
+        sys.exit(0)
--- syncs/git/blame/entrypoint.sh
+++ syncs/git/blame/entrypoint.sh
@@ -10,3 +10,12 @@
 # This script uses git blame to list blame data for all files in the repository.
 #
 # @author: Riyaz Ali (riyaz@mergestat.com)
+
+# using git ls-tree and jq find all blobs to iterate over
+git ls-tree --format='{"type": "%(objecttype)", "path": "%(path)"}' -r HEAD | jq -r 'select(.type == "blob") | .path' > blobs.txt;
+
+(while read -r path; do python /blame.py $path; done < blobs.txt) \
+  | jq -rc '[env.MERGESTAT_REPO_ID, .email, .name, .time, .hash, .line_number, .line, .path] | @csv' \
+  | psql $MERGESTAT_POSTGRES_URL -1 \
+      -c "DELETE FROM public.git_blame WHERE repo_id = '$MERGESTAT_REPO_ID'" \
+      -c "\copy public.git_blame (repo_id, author_email, author_name, author_when, commit_hash, line_no, line, path) FROM stdin (FORMAT csv)";
