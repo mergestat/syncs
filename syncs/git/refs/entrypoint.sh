5d875c7f7d4174a501d85c60e81f7dae10e5a016
Riyaz Ali <riyaz@mergestat.com>
2023-03-17 16:27:54 +0530

    sync: add git refs syncer

---
+++ syncs/git/refs/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -euo pipefail
+
+#                                    _        _
+#  _ __ ___   ___ _ __ __ _  ___ ___| |_ __ _| |_
+# | '_ ` _ \ / _ | '__/ _` |/ _ / __| __/ _` | __|
+# | | | | | |  __| | | (_| |  __\__ | || (_| | |_
+# |_| |_| |_|\___|_|  \__, |\___|___/\__\__,_|\__|
+#                     |___/
+#
+# This syncer uses mergestat-lite to sync git refs for the given repository.
+#
+# @author: Riyaz Ali (riyaz@mergestat.com)
+
+
+mergestat --format json 'SELECT *, COALESCE(COMMIT_FROM_TAG(tag), hash) AS tag_commit_hash FROM refs' \
+  | jq -rc '.[] | [env.MERGESTAT_REPO_ID, .full_name, .name, .hash, .remote, .target, .type, .tag_commit_hash] | @csv' \
+  | psql $MERGESTAT_POSTGRES_URL -1 \
+      -c "DELETE FROM public.git_refs WHERE repo_id = '$MERGESTAT_REPO_ID'" \
+      -c "\copy public.git_refs (repo_id, full_name, name, hash, remote, target, type, tag_commit_hash) FROM stdin (FORMAT csv)";
