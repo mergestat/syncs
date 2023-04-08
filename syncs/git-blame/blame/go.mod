700a0a74a01e3a82ee05160ca177052a0b77d62b
Patrick DeVivo <patrick.devivo@gmail.com>
2023-04-08 16:26:16 -0400

    feat: refactor `git-blame` sync implementation
    - swap python for go (using `gitutils` package)
    - clean up script to exec in single tx

---
+++ syncs/git-blame/blame/go.mod
@@ -0,0 +1,5 @@
+module github.com/mergestat/syncs/syncs/git-blame/blame
+
+go 1.20
+
+require github.com/mergestat/gitutils v0.0.0-20230324140435-657b0810a921
--- syncs/git-blame/entrypoint.sh
+++ syncs/git-blame/entrypoint.sh
@@ -11,17 +11,16 @@
 #
 # @author: Riyaz Ali (riyaz@mergestat.com)
 
+set -e
+
 # apply postgresql schema for the syncer
 psql $MERGESTAT_POSTGRES_URL -1 --quiet --file /syncer/schema.sql
 
-# cd into the repo directory
-cd /mergestat/repo
+# run the sync implementation
+git-blame-to-csv /mergestat/repo blame.csv
 
-# using git ls-tree and jq find all blobs to iterate over
-git ls-tree --format='{"type": "%(objecttype)", "path": "%(path)"}' -r HEAD | jq -r 'select(.type == "blob") | .path' > blobs.txt;
-
-(while read -r path; do python /syncer/blame.py $path; done < blobs.txt) \
-  | jq -rc '[env.MERGESTAT_REPO_ID, .email, .name, .time, .hash, .line_number, .line, .path] | @csv' \
-  | psql $MERGESTAT_POSTGRES_URL -1 \
-      -c "DELETE FROM public.git_blame WHERE repo_id = '$MERGESTAT_REPO_ID'" \
-      -c "\copy public.git_blame (repo_id, author_email, author_name, author_when, commit_hash, line_no, line, path) FROM stdin (FORMAT csv)";
+# load the data into postgres
+cat blame.csv | psql $MERGESTAT_POSTGRES_URL -1 \
+  -c "\set ON_ERROR_STOP on" \
+  -c "DELETE FROM public.git_blame WHERE repo_id = '$MERGESTAT_REPO_ID'" \
+  -c "\copy public.git_blame (repo_id, author_email, author_name, author_when, commit_hash, line_no, line, path) FROM stdin (FORMAT csv)"
