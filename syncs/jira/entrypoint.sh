a0e0789fcd8b5cada2250e83591f63acf8370074
Riyaz Ali <riyaz@mergestat.com>
2023-05-25 23:38:26 +0530

    fix: trim file content and compare state

--- syncs/jira/entrypoint.sh
+++ syncs/jira/entrypoint.sh
@@ -4,12 +4,24 @@ set -euo pipefail
 export NO_COLOR=true
 export MELTANO_ENVIRONMENT=mergestat
 
+# Trim leading and trailing whitespaces
+trim() {
+  local var="$*"
+  var="${var#"${var%%[![:space:]]*}"}"   # remove leading whitespaces
+  var="${var%"${var##*[![:space:]]}"}"   # remove trailing whitespaces
+  echo -n "$var"
+}
+
 # setup table schema
 psql $MERGESTAT_POSTGRES_URL --quiet -1 -c 'CREATE TABLE IF NOT EXISTS mergestat.tap_states(repo UUID, tap TEXT, state JSON, PRIMARY KEY(repo, tap), FOREIGN KEY (repo) REFERENCES public.repos (id))';
 
 # fetch and restore json state
 psql $MERGESTAT_POSTGRES_URL --quiet -t -c "SELECT state FROM mergestat.tap_states WHERE repo = '$MERGESTAT_REPO_ID' AND tap = 'mergestat:tap-jira-to-target-postgres'" > .meltano/_state.json
-[ -s ".meltano/_state.json" ] && meltano state set mergestat:tap-jira-to-target-postgres --input-file .meltano/_state.json --force
+
+# Load state from file
+if [ -s '.meltano/_state.json' ] && [ -n "$(trim "$(cat '.meltano/_state.json')")" ]; then  
+  cat '.meltano/_state.json' | meltano state set mergestat:tap-jira-to-target-postgres --input-file /dev/stdin --force
+fi
 
 meltano run tap-jira target-postgres
 
