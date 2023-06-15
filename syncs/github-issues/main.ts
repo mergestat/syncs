70a33616276ec0fe9e4f9141ae3870b05598aaeb
Josue Lopez <amenowanna@hotmail.com>
2023-06-15 09:51:02 -0500

    chore: pinning esm version

--- syncs/github-issues/main.ts
+++ syncs/github-issues/main.ts
@@ -8,8 +8,8 @@
 //
 // @author: Patrick DeVivo (patrick@mergestat.com) 
 
-import { Octokit } from "https://esm.sh/octokit@2.0.14";
-import { paginateGraphql } from "https://esm.sh/@octokit/plugin-paginate-graphql@2.0.1";
+import { Octokit } from "https://esm.sh/v124/octokit@2.0.14";
+import { paginateGraphql } from "https://esm.sh/v124/@octokit/plugin-paginate-graphql@2.0.1";
 import { Client } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
 
 const query = await Deno.readTextFile("./query.gql");
