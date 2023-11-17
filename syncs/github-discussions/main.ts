50ae720b7580d5edadfe28305e85213c1dc320d7
Patrick DeVivo <patrick.devivo@gmail.com>
2023-11-17 16:29:42 -0500

    feat: implement `github_discussion_categories`
    and `github_discussion_comments`

--- syncs/github-discussions/main.ts
+++ syncs/github-discussions/main.ts
@@ -13,7 +13,7 @@ import { paginateGraphql } from "https://cdn.jsdelivr.net/npm/@octokit/plugin-pa
 import { throttling } from "https://esm.sh/@octokit/plugin-throttling";
 import { Client } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
 
-const discussionsQuery = await Deno.readTextFile("./discussions_query.gql");
+const discussionsQuery = await Deno.readTextFile("./query.gql");
 const params = JSON.parse(Deno.env.get("MERGESTAT_PARAMS") || "{}");
 const repoID = Deno.env.get("MERGESTAT_REPO_ID")
 const repoURL = new URL(Deno.env.get("MERGESTAT_REPO_URL") || "");
@@ -61,6 +61,19 @@ for await (const response of iterator) {
 
 console.log(`fetched ${discussionsBuffer.length} discussions for: ${owner}/${repo}`)
 
+
+const discussionCategories: any = {};
+const discussionComments = [];
+// now iterate over every discussion object to pull out entries for
+// github_discussion_categories and github_discussion_comments
+for (const discussion of discussionsBuffer) {
+  discussionCategories[discussion.category?.id] = discussion.category;
+  
+  for (const comment of discussion.comments.nodes) {
+    discussionComments.push({ discussionID: discussion.id, ...comment });
+  }
+}
+
 const schemaSQL = await Deno.readTextFile("./schema.sql");
 const client = new Client(Deno.env.get("MERGESTAT_POSTGRES_URL"));
 await client.connect();
@@ -72,9 +85,42 @@ await tx.queryArray(schemaSQL);
 await tx.queryArray(`DELETE FROM public.github_discussions WHERE repo_id = $1;`, [repoID]);
 for await (const discussion of discussionsBuffer) {
     await tx.queryArray(`
-INSERT INTO public.github_discussions (repo_id, id, active_lock_reason, is_answered, answer_id, answer_chosen_at, answer_chosen_by, author_login, author_association, body_text, category, comment_count, created_at, created_via_email, database_id, editor_login, last_edited_at, locked, number, published_at, reaction_count, title, updated_at, url)
+INSERT INTO public.github_discussions (repo_id, id, active_lock_reason, is_answered, answer_id, answer_chosen_at, answer_chosen_by, author_login, author_association, body, category, comment_count, created_at, created_via_email, database_id, editor_login, last_edited_at, locked, number, published_at, reaction_count, title, updated_at, url)
 VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24)
-    `, [repoID, discussion.id, discussion.activeLockReason, discussion.isAnswered, discussion.answer?.id, discussion.answerChosenAt, discussion.answerChosenBy?.login, discussion.author?.login, discussion.authorAssociation, discussion.bodyText, discussion.category?.name, discussion.comments?.totalCount, discussion.createdAt, discussion.createdViaEmail, discussion.databaseId, discussion.editor?.login, discussion.lastEditedAt, discussion.locked, discussion.number, discussion.publishedAt, discussion.reactions?.totalCount, discussion.title, discussion.updatedAt, discussion.url]);
+    `, [repoID, discussion.id, discussion.activeLockReason, discussion.isAnswered, discussion.answer?.id, discussion.answerChosenAt, discussion.answerChosenBy?.login, discussion.author?.login, discussion.authorAssociation, discussion.body, discussion.category?.name, discussion.comments?.totalCount, discussion.createdAt, discussion.createdViaEmail, discussion.databaseId, discussion.editor?.login, discussion.lastEditedAt, discussion.locked, discussion.number, discussion.publishedAt, discussion.reactions?.totalCount, discussion.title, discussion.updatedAt, discussion.url]);
+}
+
+await tx.queryArray(`DELETE FROM public.github_discussion_categories WHERE repo_id = $1;`, [repoID]);
+for (const id in discussionCategories) {
+  const category = discussionCategories[id];
+  await tx.queryArray(`
+  INSERT INTO public.github_discussion_categories (repo_id, id, name, description, created_at, emoji, is_answerable, slug, updated_at)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);`,
+  [repoID, id, category.name, category.description, category.createdAt, category.emoji, category.isAnswerable, category.slug, category.updatedAt]);
+}
+
+await tx.queryArray(`DELETE FROM public.github_discussion_comments WHERE repo_id = $1;`, [repoID]);
+for await (const comment of discussionComments) {
+  await tx.queryArray(`
+  INSERT INTO public.github_discussion_comments (repo_id, id, discussion_id, author_login, author_association, body, created_at, deleted_at, is_minimized, minimized_reason, reaction_count, reply_to_id, updated_at, upvote_count, url)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)
+  `, [
+    repoID,
+    comment.id,
+    comment.discussionID,
+    comment.author?.login,
+    comment.authorAssociation,
+    comment.body,
+    comment.createdAt,
+    comment.deletedAt,
+    comment.isMinimized,
+    comment.minimizedReason,
+    comment.reactions?.totalCount,
+    comment.replyTo?.id,
+    comment.updatedAt,
+    comment.upvoteCount,
+    comment.url
+  ]);
 }
 
 await tx.commit();
--- syncs/github-discussions/schema.sql
+++ syncs/github-discussions/schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS github_discussions (
     answer_chosen_by text,
     author_login text,
     author_association text,
-    body_text text,
+    body text,
     category text,
     comment_count int,
     created_at timestamp with time zone,
@@ -30,38 +30,44 @@ CREATE TABLE IF NOT EXISTS github_discussions (
 
 COMMENT ON TABLE github_discussions IS 'discussions of a GitHub repo';
 COMMENT ON COLUMN github_discussions.repo_id IS 'foreign key for public.repos.id';
--- COMMENT ON COLUMN github_issues.author_login IS 'login of the author of the issue';
--- COMMENT ON COLUMN github_issues.body IS 'body of the issue';
--- COMMENT ON COLUMN github_issues.closed IS 'boolean to determine if the issue is closed';
--- COMMENT ON COLUMN github_issues.closed_at IS 'timestamp of when the issue was closed';
--- COMMENT ON COLUMN github_issues.created_via_email IS 'boolean to determine if the issue was created via email';
--- COMMENT ON COLUMN github_issues.database_id IS 'GitHub database_id of the issue';
--- COMMENT ON COLUMN github_issues.editor_login IS 'login of the editor of the issue';
--- COMMENT ON COLUMN github_issues.includes_created_edit IS 'boolean to determine if the issue was edited and includes an edit with the creation data';
--- COMMENT ON COLUMN github_issues.label_count IS 'number of labels associated to the issue';
--- COMMENT ON COLUMN github_issues.last_edited_at IS 'timestamp when the issue was edited';
--- COMMENT ON COLUMN github_issues.locked IS 'boolean to determine if the issue is locked';
--- COMMENT ON COLUMN github_issues.milestone_count IS 'number of milestones associated to the issue';
--- COMMENT ON COLUMN github_issues.number IS 'GitHub number for the issue';
--- COMMENT ON COLUMN github_issues.participant_count IS 'number of participants associated to the issue';
--- COMMENT ON COLUMN github_issues.published_at IS 'timestamp when the issue was published';
--- COMMENT ON COLUMN github_issues.reaction_count IS 'number of reactions associated to the issue';
--- COMMENT ON COLUMN github_issues.state IS 'state of the issue';
--- COMMENT ON COLUMN github_issues.title IS 'title of the issue';
--- COMMENT ON COLUMN github_issues.updated_at IS 'timestamp when the issue was updated';
--- COMMENT ON COLUMN github_issues.url IS 'GitHub URL of the issue';
--- COMMENT ON COLUMN github_issues.labels IS 'labels associated to the issue';
--- COMMENT ON COLUMN github_issues._mergestat_synced_at IS 'timestamp when record was synced into the MergeStat database';
+-- TODO(patrickdevivo) describe other columns
 
 CREATE INDEX IF NOT EXISTS idx_github_discussions_repo_id_fkey ON github_discussions(repo_id uuid_ops);
 
--- ----
--- CREATE TABLE IF NOT EXISTS github_discussion_comments (
---     repo_id uuid REFERENCES repos(id) ON DELETE CASCADE ON UPDATE RESTRICT,
+CREATE TABLE IF NOT EXISTS github_discussion_categories (
+    repo_id uuid REFERENCES repos(id) ON DELETE CASCADE ON UPDATE RESTRICT,
+    id text,
+    name text,
+    description text,
+    created_at timestamp with time zone,
+    emoji text,
+    is_answerable boolean,
+    slug text,
+    updated_at timestamp with time zone,
 
---     _mergestat_synced_at timestamp with time zone NOT NULL DEFAULT now(),
---     CONSTRAINT github_discussion_comments_pkey PRIMARY KEY (repo_id, database_id)
--- );
+    _mergestat_synced_at timestamp with time zone NOT NULL DEFAULT now(),
+    CONSTRAINT github_discussion_categories_pkey PRIMARY KEY (repo_id, id)
+);
 
--- COMMENT ON TABLE github_discussion_comments IS 'discussion comments of a GitHub repo';
--- COMMENT ON COLUMN github_discussion_comments.repo_id IS 'foreign key for public.repos.id';
+CREATE INDEX IF NOT EXISTS idx_github_discussion_categories_repo_id_fkey ON github_discussions(repo_id uuid_ops);
+
+CREATE TABLE IF NOT EXISTS github_discussion_comments (
+    repo_id uuid REFERENCES repos(id) ON DELETE CASCADE ON UPDATE RESTRICT,
+    id text,
+    discussion_id text,
+    author_login text,
+    author_association text,
+    body text,
+    created_at timestamp with time zone,
+    deleted_at timestamp with time zone,
+    is_minimized boolean,
+    minimized_reason text,
+    reaction_count int,
+    reply_to_id text,
+    updated_at timestamp with time zone,
+    upvote_count int,
+    url text,
+
+    _mergestat_synced_at timestamp with time zone NOT NULL DEFAULT now(),
+    CONSTRAINT github_discussion_commentss_pkey PRIMARY KEY (repo_id, id)
+);
